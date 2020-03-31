e5184452b03008e749a0b7f4005e69c46f7992c6
Mateus Henrique <mateus_hg@hotmail.com>
2020-03-31 19:10:12 +0200

    navbar

---
+++ src/app/navmenu/menu.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+@NgModule({
+  declarations: [
+
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    FormsModule,
+    HttpClientModule,
+  ]
+})
+export class MenuModule { }
